8584cfc56bd1efe7dfc2d5a4393d0cf764da2c76
Yjunha <junay00v@gmail.com>
2024-07-04 19:00:47 +0900

    Section 5. 싱글톤 컨테이너 @Configuration과 바이트코드 조작의 마법

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -30,9 +30,6 @@ public class AppConfig {
     //call AppConfig.memberRepository
     //call AppConfig.orderService
 
-
-
-
     @Bean
     public MemberService memberService() {
         System.out.println("call AppConfig.memberService");
