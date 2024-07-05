4195fbe06fd172033d53c1a5d040f90561dd550b
Yjunha <junay00v@gmail.com>
2024-07-05 16:40:11 +0900

    Feat: domain 패키지에 Member 클래스 추가

---
+++ assignment/assignment1/src/main/java/myAssignment/assignment1/domain/Member.java
@@ -0,0 +1,12 @@
+package myAssignment.assignment1.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+
+    private String id;
+    private String password;
+}
