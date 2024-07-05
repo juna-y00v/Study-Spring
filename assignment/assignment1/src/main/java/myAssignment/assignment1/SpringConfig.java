4f584cda2ba83ea2068587ea29547bb18d3ce550
Yjunha <junay00v@gmail.com>
2024-07-05 22:26:55 +0900

    Feat: ADD Spring Config

---
+++ assignment/assignment1/src/main/java/myAssignment/assignment1/SpringConfig.java
@@ -0,0 +1,19 @@
+package myAssignment.assignment1;
+
+
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private final DataSource dataSource;
+    private final EntityManager em;
+
+    public SpringConfig(DataSource dataSource, EntityManager em) {
+        this.dataSource = dataSource;
+        this.em = em;
+    }
+}
--- assignment/assignment1/src/main/java/myAssignment/assignment1/repository/JpaMemberRepository.java
+++ assignment/assignment1/src/main/java/myAssignment/assignment1/repository/JpaMemberRepository.java
@@ -3,14 +3,18 @@ package myAssignment.assignment1.repository;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import myAssignment.assignment1.domain.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
 
+@Component
 public class JpaMemberRepository implements MemberRepository {
 
     private final EntityManager em;
 
+    @Autowired
     public JpaMemberRepository(EntityManager em) {
         this.em = em;
     }
