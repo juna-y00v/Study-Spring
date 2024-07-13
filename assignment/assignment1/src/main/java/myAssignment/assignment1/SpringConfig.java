14d6333770d08202441b85121c5a9f9f80c2c9f0
Yjunha <junay00v@gmail.com>
2024-07-14 03:56:01 +0900

    SpringConfig DI 설정

--- assignment/assignment1/src/main/java/myAssignment/assignment1/SpringConfig.java
+++ assignment/assignment1/src/main/java/myAssignment/assignment1/SpringConfig.java
@@ -1,24 +1,33 @@
 package myAssignment.assignment1;
 
+import jakarta.persistence.EntityManager;
+import myAssignment.assignment1.repository.JpaMemberRepository;
 import myAssignment.assignment1.repository.MemberRepository;
 import myAssignment.assignment1.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class SpringConfig {
 
-    private final MemberRepository memberRepository;
+    private final EntityManager em;
 
     @Autowired
-    public SpringConfig(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository);
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new JpaMemberRepository(em);
     }
 }
+
 
