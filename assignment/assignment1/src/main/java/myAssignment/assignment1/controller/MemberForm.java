a153cc59c128a33d46a18d4ecf29f5623442238d
Yjunha <junay00v@gmail.com>
2024-07-14 17:40:05 +0900

    생성 MemberForm

---
+++ assignment/assignment1/src/main/java/myAssignment/assignment1/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package myAssignment.assignment1.controller;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberForm {
+
+    private String userId;
+    private String password;
+}
