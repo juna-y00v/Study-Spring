889b6f48cb1bca0ecd2a4933c83977ad16608ed9
Yjunha <junay00v@gmail.com>
2024-07-14 15:36:30 +0900

    추가 JpaMemberRepositroy 저장 기능 구현

---
+++ assignment/assignment1/src/main/java/myAssignment/assignment1/repository/JpaMemberRepository.java
@@ -0,0 +1,19 @@
+package myAssignment.assignment1.repository;
+
+import jakarta.persistence.EntityManager;
+import myAssignment.assignment1.domain.Member;
+
+public class JpaMemberRepository implements MemberRepository {
+
+    private final EntityManager em;
+
+    public JpaMemberRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+}
