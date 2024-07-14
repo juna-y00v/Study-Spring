37b9c6c1f64895ef20f2b2812666cc4aea6842f1
Yjunha <junay00v@gmail.com>
2024-07-14 17:41:43 +0900

    생성 MemberController 회원 가입 맵핑

---
+++ assignment/assignment1/src/main/java/myAssignment/assignment1/controller/MemberController.java
@@ -0,0 +1,34 @@
+package myAssignment.assignment1.controller;
+
+import myAssignment.assignment1.domain.Member;
+import myAssignment.assignment1.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setUserId(form.getUserId());
+        member.setPassword(form.getPassword());
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+}
