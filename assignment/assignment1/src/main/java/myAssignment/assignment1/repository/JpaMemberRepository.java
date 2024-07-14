07baf74777467eb4e0010430e66360be682e5946
Yjunha <junay00v@gmail.com>
2024-07-14 18:36:40 +0900

    추가 아이디 찾기 기능

--- assignment/assignment1/src/main/java/myAssignment/assignment1/repository/JpaMemberRepository.java
+++ assignment/assignment1/src/main/java/myAssignment/assignment1/repository/JpaMemberRepository.java
@@ -3,6 +3,9 @@ package myAssignment.assignment1.repository;
 import jakarta.persistence.EntityManager;
 import myAssignment.assignment1.domain.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 public class JpaMemberRepository implements MemberRepository {
 
     private final EntityManager em;
@@ -15,5 +18,14 @@ public class JpaMemberRepository implements MemberRepository {
     public Member save(Member member) {
         em.persist(member);
         return member;
+    }
+
+    @Override
+    public Optional<Member> findByUserId(String userId) {
+        List<Member> result = em.createQuery("select m from Member m where m.userId = :userid", Member.class)
+                .setParameter("userid", userId)
+                .getResultList();
+
+        return result.stream().findAny();
     }
 }
--- assignment/assignment1/src/main/java/myAssignment/assignment1/repository/MemberRepository.java
+++ assignment/assignment1/src/main/java/myAssignment/assignment1/repository/MemberRepository.java
@@ -2,7 +2,11 @@ package myAssignment.assignment1.repository;
 
 import myAssignment.assignment1.domain.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository {
 
     Member save(Member member);
+
+    Optional<Member> findByUserId(String userId);
 }
