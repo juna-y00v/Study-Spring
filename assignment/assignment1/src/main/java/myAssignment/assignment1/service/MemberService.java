052d27a2cc784b706b30b88a094f110dc528021b
Yjunha <junay00v@gmail.com>
2024-07-14 15:40:46 +0900

    멤버 서비스 구현체 생성 회원가입 구현

---
+++ assignment/assignment1/src/main/java/myAssignment/assignment1/service/MemberService.java
@@ -0,0 +1,19 @@
+package myAssignment.assignment1.service;
+
+import myAssignment.assignment1.domain.Member;
+import myAssignment.assignment1.repository.MemberRepository;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    // 회원 가입
+    public Long join(Member member) {
+        memberRepository.save(member);
+        return member.getId();
+    }
+}
