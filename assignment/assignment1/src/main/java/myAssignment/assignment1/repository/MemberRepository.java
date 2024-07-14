ac96a3e597cebb5c28c982510569eef5c8ecf305
Yjunha <junay00v@gmail.com>
2024-07-14 15:29:01 +0900

    delete

--- assignment/assignment1/src/main/java/myAssignment/assignment1/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package myAssignment.assignment1.repository;
-
-import myAssignment.assignment1.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member,Long> {
-
-}
