80de0def8e3de6bfeae15ff422e85e25473653b2
Yjunha <junay00v@gmail.com>
2024-07-14 15:42:07 +0900

    생성 HomeController

---
+++ assignment/assignment1/src/main/java/myAssignment/assignment1/controller/HomeController.java
@@ -0,0 +1,13 @@
+package myAssignment.assignment1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
